374bc4ccf7ff1d794420b0d57d67aa87baac9d4d
Samantha Turbeville <smturbev@uw.edu>
2022-05-04 00:28:48 +0200

    diurnal cycle with hourmean

--- sh_scripts/get_TWP_native.sh
+++ sh_scripts/get_TWP_native.sh
@@ -22,6 +22,7 @@ LOC="TWP"
 IN_PATH=/work/dicad/cmip6-dev/data4freva/model/global/dyamond/DYAMOND_WINTER
 
 ##### GEOS #####
+## something wrong with GEOS file
 # OUT_PATH=/scratch/b/b380883/dyamond2/GEOS/$LOC
 # MODEL_PATH=NASA/GEOS-6km # AORI/NICAM-3km SBU/SAM2-4km/
 # declare -a VarArray15min=(rsdt rsut clivi rlut rlutcs)
@@ -34,18 +35,19 @@ IN_PATH=/work/dicad/cmip6-dev/data4freva/model/global/dyamond/DYAMOND_WINTER
 # done
 
 ##### SAM2 #####
-OUT_PATH=/scratch/b/b380883/dyamond2/SAM/$LOC
-MODEL_PATH=SBU/SAM2-4km/
-declare -a VarArray15min=(rltacc rstacc clivi)
+## done
+# OUT_PATH=/scratch/b/b380883/dyamond2/SAM/$LOC
+# MODEL_PATH=SBU/SAM2-4km/
+# declare -a VarArray15min=(rltacc rstacc clivi)
 
-for v in "${VarArray15min[@]}"; do
-    for f in $IN_PATH/$MODEL_PATH/DW-ATM/atmos/15min/$v/r1i1p1f1/2d/gn/*; do
-        fname=$(basename $f)
-        out_file=$OUT_PATH/$fname
-        echo "SAM2 15 min variable "$v":"
-        cdo -f nc4 -sellonlatbox,$LON0,$LON1,$LAT0,$LAT1 $f $out_file  
-    done
-done
+# for v in "${VarArray15min[@]}"; do
+#     for f in $IN_PATH/$MODEL_PATH/DW-ATM/atmos/15min/$v/r1i1p1f1/2d/gn/*; do
+#         fname=$(basename $f)
+#         out_file=$OUT_PATH/$fname
+#         echo "SAM2 15 min variable "$v":"
+#         cdo -f nc4 -sellonlatbox,$LON0,$LON1,$LAT0,$LAT1 $f $out_file  
+#     done
+# done
 
 ##### NICAM #####
 OUT_PATH=/scratch/b/b380883/dyamond2/NICAM/$LOC
@@ -53,7 +55,7 @@ MODEL_PATH=AORI/NICAM-3km
 declare -a VarArray15min=(clivi rlut rsut rsdt)
 
 for v in "${VarArray15min[@]}"; do
-    for f in $IN_PATH/$MODEL_PATH/DW-CPL/atmos/15mn/$v/r1i1p1f1/2d/gn/*; do
+    for f in $IN_PATH/$MODEL_PATH/DW-ATM/atmos/15min/$v/r1i1p1f1/2d/gn/*; do
         fname=$(basename $f)
         out_file=$OUT_PATH/$fname
         echo "NICAM 15 min variable "$v":"
