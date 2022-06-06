4dede6b6622fbf81fba1ffbf4806fb3095e7912f
Samantha Turbeville <b380883@l40100.atos.local>
2022-06-06 20:58:54 +0200

    added colors dict from json

--- python_scripts/utility/analysis_parameters.py
+++ python_scripts/utility/analysis_parameters.py
@@ -7,6 +7,14 @@ especially if file names change
 """
 import numpy as np
 import xarray as xr
+import json
+
+# Color scheme from dyamond1
+COLOR_JSON = "/pf/b/b380883/dyamond2/dyamond_colors.json"
+def js_r(filename):
+    with open(filename) as f_in:
+        return(json.load(f_in))
+COLORS = js_r(COLOR_JSON)
 
 # data #
 CERES_SYN1_1H = "/work/bb1153/b380883/TWP/CERES_SYN1deg-1H_Terra-Aqua-MODIS_Ed4.1_Subset_20200101-20200331.nc"
@@ -19,7 +27,7 @@ GT=SCR2+"GT/"
 TWP = SCR2+"TWP/"
 
 ## time mean ##
-TIMMEAN = ""
+TIMMEAN_GT = GT+"timmean/"
 
 def get_twp_file(model, var):
     if model.lower()=="data" or model.lower()=="ceres":
@@ -50,104 +58,24 @@ def get_gt_file(model, var):
         return
 
 def get_timmean_file(model, var, gt=True):
-    if gt:
-        if (model.lower()=="geos") or (model.lower()=="sam"):
-            return TIMMEAN+"timmean_GT_{m}_{v}_20200120-20200229.nc".format(m=model, v=var)
-        elif (model.lower()=="nicam"):
-            return TIMMEAN+"timmean_GT_{m}_{v}_20200120-20200228.nc".format(m=model, v=var)
-        elif (model.lower()=="screamr"):
-            return TIMMEAN+"timmean_GT_{m}r_{v}_20200120-20200301.nc".format(m=model, v=var)
-        elif (model.lower()=="scream"):
-            return TIMMEAN+"timmean_GT_{m}_{v}_20200120-20200229.nc".format(m=model, v=var)
-        else:
-            raise Exception("timemean for "+model+" & "+var+" for GT not accepted.")
-            return
-    else:
-        if (model.lower()=="nicam"):
-            return TIMMEAN+"timmean_ITCZ_{m}_{v}_20200120-20200228.nc".format(m=model, v=var)
-        else:
-            raise Exception("timemean for "+model+" & "+var+" for GT not accepted.")
-            return
-
-def test_data_file_name(var, date=None, native=False):
-    """returns file name of desired file from test data
-
-    Input:
-        - var (str): h1-9
-        - date(str): format mm-dd ranges from 01-20 to 03-01 
-
-    h0 : 15 min : CAPE, CIN, CLDHGH, CLDLOW, CLDMED, CLDTOT,
-    TMCLDICE (INT CLD ICE), TMCLDRIM (?), TMQ (?), TMRAINQM (INT RAIN)
-    h1 : 15 min : LHFLX, PRECSL (large scale precip m/s), PRECT (tot precip m/s), PS (surf pressure Pa), 
-    QREFHT (reference), TAUX and TAUY (windstress), TREFHT, TS, WINDSPD_10M
-    h2 : 15 min : FLDS (downwelling lw sfc), FLNS (net lw sfc), FLNT (net lw toa), 
-    FLNTC (clearsky lw toa), FSDS (sw downwelling sfc), FSNS (net sw sfc), 
-    FSNTOA (sw net toa), FSNTOAC (clearsky sw toa)
-    h3 : 15 min : OMEGA200, 500, 700, 850; RH200, 500, 700, 850; Z200, 500, 700,850
-    h4 : 3 hrly : TMNUMICE, TMNUMLIQ, TMNUMRAI
-    h5 : 3 hrly : U, V
-    h6 : 3 hrly : Q (mixing ratio vapor kg/kg), T (temp K)
-    h7 : 3 hrly : CLDICE (kg/kg), CLDLIQ (kg/kg)
-    h8 : 3 hrly : CLOUD (fraction), OMEGA (Pa/s)
-    h9 : 3 hrly : EMIS (cloud emissivity?), TOT_CLD_VISTAU (cloud optical depth in the visual)
-    swd: 15 min : incoming solar radiaiton
-    native only has two possible dates available 
-    2020-01-20-00000 and 2020-02-17-00000 for h0 and h9 respectively
-    """
-    if native:
-        # only two possible dates available 2020-01-20-00000 and 2020-02-17-00000 for h0 and h9 respectively
-        if var=="h0":
-            return native_dir + "SCREAMv0.SCREAM-DY2.ne1024pg2.20201127.eam.h0.2020-01-20-00000.nc"
-        elif var=="h9":
-            return native_dir + "SCREAMv0.SCREAM-DY2.ne1024pg2.20201127.eam.h9.2020-02-17-00000.nc"
-        else:
-            raise Exception("exploratory data only use var = 'h0' or 'h9', date can be None")
-    else:
-        if date is None:
-            raise Exception("Must include a date as a string in the format of mm-dd in range 01-20 to 03-01 (Jan 20 to March 1)")
-        elif date=="all":
-            if (var=="swd") or (var=="solin"):
-                return "/home/disk/eos12/hillmanb/scream/dyamond2/ne30pg2_256x512/SCREAMv0.SCREAM-DY2.ne30pg2_ne30pg2.20220208.eam.h2.2020*.256x512.nc"
+    if var=="clt":
+        if gt:
+            if (model.lower()=="geos") or model.lower()=="scream" or model.lower()=="icon":
+                return TIMMEAN_GT+"timmean_GT_{m}_{v}_20200130-20200301.nc".format(m=model, v=var)
+            elif (model.lower()=="nicam")or (model.lower()=="sam") or model.lower()=="um":
+                return TIMMEAN_GT+"timmean_GT_{m}_{v}_20200130-20200228.nc".format(m=model, v=var)
+            elif (model.lower()=="screamr"):
+                return TIMMEAN_GT+"timmean_GT_{m}r_{v}_20200130-20200301.nc".format(m=model, v=var)
             else:
-                return coarse_dir + f"SCREAMv0.SCREAM-DY2.ne1024pg2.20201127.eam.{var}.2020*00000.nc".format(var=var)
+                raise Exception("timemean for "+model+" & "+var+" for GT not accepted.")
+                return
         else:
-            if (var=="swd") or (var=="solin"):
-                return f"/home/disk/eos12/hillmanb/scream/dyamond2/ne30pg2_256x512/SCREAMv0.SCREAM-DY2.ne30pg2_ne30pg2.20220208.eam.h2.2020-{date}-00000.256x512.nc".format(date=date)
+            if (model.lower()=="nicam"):
+                raise Exception("timemean for "+model+" & "+var+" for TWP not accepted.")
+                return 
             else:
-                return coarse_dir + f"SCREAMv0.SCREAM-DY2.ne1024pg2.20201127.eam.{var}.2020-{date}-00000.nc".format(var=var, date=date)
-        
-def return_ceres(time="hourly"):
-    if time=="hourly":
-        return ceres_dir + "CERES_SYN1deg-1H_Terra-Aqua-MODIS_Ed4.1_Subset_20200101-20200331.nc"
-    elif time=="daily":
-        return ceres_dir + "CERES_SYN1deg-Day_Terra-Aqua-MODIS_Ed4.1_Subset_20200101-20200331.nc"
+                raise Exception("timemean for "+model+" & "+var+" for TWP not accepted.")
+                return
     else:
-        raise Exception("only have 'hourly' or 'daily' data available for ceres syn 1")
-        
-def load_dates(h, lats=(-30, 30), lons=(None, None)):
-    lat0, lat1 = lats
-    lon0, lon1 = lons               
-    feb = [ "02-%02d"%(i+1) for i in range(28) ]
-    dates = ["01-30", "01-31"]+ feb
-    if h!="swd":
-        ds = xr.open_dataset(test_data_file_name(h, dates[0])).drop(["P3_input_dim","P3_output_dim","ilev","lev","swband","lwband", "slat","slon"])
-    else:
-        ds = xr.open_dataset(test_data_file_name(h, dates[0]))
-    if lon0 is None:
-        ds = ds.sel(lat=slice(lat0, lat1))
-    else:
-        ds = ds.sel(lat=slice(lat0, lat1)).sel(lon=slice(lon0,lon1))
-
-    for date in dates[1:]:
-        file = test_data_file_name(h, date)
-        print(date, end=", ")
-        if h!="swd":
-            ds_1 = xr.open_dataset(file).drop(["P3_input_dim","P3_output_dim","swband","lwband", "slat","slon"])
-        else:
-            ds_1 = xr.open_dataset(file)
-        if lon0 is None:
-            ds_1 = ds_1.sel(lat=slice(lat0, lat1))
-        else:
-            ds_1 = ds_1.sel(lat=slice(lat0, lat1)).sel(lon=slice(lon0,lon1))
-        ds = xr.combine_by_coords([ds, ds_1])
-    return ds
+        raise Exception("timemean for "+var+" not accepted.")
+        return
