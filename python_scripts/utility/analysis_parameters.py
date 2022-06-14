b2e83e0b30a484e91e1e24c75e0ebf86c55eef65
Samantha <smturbev@uw.edu>
2022-06-14 10:16:10 +0200

    added colors and get_file to include regions

--- python_scripts/utility/analysis_parameters.py
+++ python_scripts/utility/analysis_parameters.py
@@ -9,8 +9,8 @@ import numpy as np
 import xarray as xr
 import json
 
-# Color scheme from dyamond1
-COLOR_JSON = "/pf/b/b380883/dyamond2/dyamond_colors.json"
+# Color scheme from dyamond
+COLOR_JSON = "/home/b/b380883/dyamond2/dyamond_colors.json"
 def js_r(filename):
     with open(filename) as f_in:
         return(json.load(f_in))
@@ -29,43 +29,48 @@ TWP = SCR2+"TWP/"
 ## time mean ##
 TIMMEAN_GT = GT+"timmean/"
 
-def get_twp_file(model, var):
-    if model.lower()=="data" or model.lower()=="ceres":
-        if var.lower()=="olr" or var.lower()=="rlut" or var.lower()=="rad" or var.lower()=="sw":
-            return CERES_SYN1_1H
-    else:
-        if model.lower()=="nicam" or model.lower()=="um":
-            return TWP+"TWP_"+model.upper()+"_"+var.lower()+"_20200130-20200228.nc"
-        elif model.lower()=="geos":
-            return TWP+"TWP_"+model.upper()+"_"+var.lower()+"_20200130-20200303.nc"
-        elif model.lower()=="scream" or model.lower()=="sam" or model.lower()=="icon":
-            return TWP+"TWP_"+model.upper()+"_"+var.lower()+"_20200130-20200301.nc"
+def get_file(model, var, region="twp"):
+    if region.lower()=="twp":
+        if model.lower()=="data" or model.lower()=="ceres":
+            if var.lower()=="olr" or var.lower()=="rlut" or var.lower()=="rad" or var.lower()=="sw":
+                return CERES_SYN1_1H
         else:
-            raise Exception("model not valid")   
-        return
-
-def get_gt_file(model, var):
-    if model.lower()=="data" or model.lower()=="ceres":
-        if var.lower()=="olr" or var.lower()=="rlut" or var.lower()=="rad" or var.lower()=="sw":
-            return 
-    else:
-        if model.lower()=="nicam" or model.lower()=="um":
-            return GT+"GT_"+model.upper()+"_"+var.lower()+"_20200130-20200228.nc"
-        elif model.lower()=="scream" or model.lower()=="sam" or model.lower()=="icon" or model.lower()=="geos":
-            return GT+"GT_"+model.upper()+"_"+var.lower()+"_20200130-20200301.nc"
+            if model.lower()=="nicam" or model.lower()=="um":
+                return TWP+"TWP_"+model.upper()+"_"+var.lower()+"_20200130-20200228.nc"
+            elif model.lower()=="geos":
+                return TWP+"TWP_"+model.upper()+"_"+var.lower()+"_20200130-20200303.nc"
+            elif model.lower()=="sam" or model.lower()=="icon" or model.lower()[:6]=="scream":
+                return TWP+"TWP_"+model.upper()+"_"+var.lower()+"_20200130-20200301.nc"
+            else:
+                raise Exception("model not valid")   
+            return
+    elif region.lower()=="gt" or region.lower()=="tropics":
+        if model.lower()=="data" or model.lower()=="ceres":
+            if var.lower()=="olr" or var.lower()=="rlut" or var.lower()=="rad" or var.lower()=="sw":
+                return 
         else:
-            raise Exception("model not valid")   
+            if model.lower()=="nicam" or model.lower()=="um":
+                return GT+"GT_"+model.upper()+"_"+var.lower()+"_20200130-20200228.nc"
+            elif model.lower()=="scream" or model.lower()=="sam" or model.lower()=="icon" or model.lower()=="geos" or model.lower()=="screamr1deg":
+                return GT+"GT_"+model.upper()+"_"+var.lower()+"_20200130-20200301.nc"
+            else:
+                raise Exception("model not valid")   
+            return
         return
+    else:
+        raise Exception("region not accepted - try TWP or GT or Tropics")
+        return
+    return
 
 def get_timmean_file(model, var, gt=True):
     if var=="clt":
         if gt:
-            if (model.lower()=="geos") or model.lower()=="scream" or model.lower()=="icon":
+            if (model.lower()=="geos") or (model.lower()=="nicam") or model.lower()=="scream" or model.lower()=="icon" or (model.lower()=="screamr"):
                 return TIMMEAN_GT+"timmean_GT_{m}_{v}_20200130-20200301.nc".format(m=model, v=var)
-            elif (model.lower()=="nicam")or (model.lower()=="sam") or model.lower()=="um":
+            elif model.lower()=="um":
                 return TIMMEAN_GT+"timmean_GT_{m}_{v}_20200130-20200228.nc".format(m=model, v=var)
-            elif (model.lower()=="screamr"):
-                return TIMMEAN_GT+"timmean_GT_{m}r_{v}_20200130-20200301.nc".format(m=model, v=var)
+            elif (model.lower()=="sam"):
+                return TIMMEAN_GT+"timmean_GT_{m}_{v}_20200130-20200229.nc".format(m=model, v=var)
             else:
                 raise Exception("timemean for "+model+" & "+var+" for GT not accepted.")
                 return
