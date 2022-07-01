367fba9b67ed30778da2699b260eb2615322088e
Samantha <smturbev@uw.edu>
2022-07-01 20:23:19 +0200

    get dem (cloud emissivity in LW)

--- sh_scripts/get_native_scream.sh
+++ sh_scripts/get_native_scream.sh
@@ -17,23 +17,35 @@ MODEL_PATH=LLNL/SCREAM-3km
 GRID_FILE=/work/dicad/from_Mistral/dicad/cmip6-dev/data4freva/model/global/dyamond/DYAMOND_WINTER/LLNL/SCREAM-3km/grid.nc
 echo $GRID_FILE
 
-LON0=0
-LON1=360
-LAT0=-30
-LAT1=30
-# LON0=143
-# LON1=153
-# LAT0=-5
-# LAT1=5
-LOC="GT"
+# LON0=0
+# LON1=360
+# LAT0=-30
+# LAT1=30
+LON0=143
+LON1=153
+LAT0=-5
+LAT1=5
+LOC="TWP"
 
 OUT_PATH=/scratch/b/b380883/
 
-declare -a VarArray15min=(pr) #rlt rst clivi)
+# declare -a VarArray15min=(qvi) #pr rlt rst clivi)
 
 # 15 min vars
 for v in "${VarArray15min[@]}"; do
-   for f in $IN_PATH/$MODEL_PATH/DW-ATM/atmos/15min/$v/r1i1p1f1/2d/gn/*2020013*; do
+    for f in $IN_PATH/$MODEL_PATH/DW-ATM/atmos/15min/$v/r1i1p1f1/2d/gn/*202002*; do
+        fname=$(basename $f)
+        out_file=$OUT_PATH/$LOC"_"$fname
+        echo "15 min variable "$v":"
+        # cdo -f nc -sellonlatbox,$LON0,$LON1,$LAT0,$LAT1 -setgrid,$GRID_FILE $f $out_file
+    done
+done
+
+declare -a VarArray3D=(dem) # dem cli clw lat lon ta hus cld-mod cl (defined as L/IWC > 10e-5) dtau icenvi cldnvi
+
+# 3 hr 3D vars
+for v in "${VarArray3D[@]}"; do
+   for f in $IN_PATH/$MODEL_PATH/DW-ATM/atmos/3hr/$v/r1i1p1f1/ml/gn/*; do
        fname=$(basename $f)
        out_file=$OUT_PATH/$LOC"_"$fname
        echo "15 min variable "$v":"
@@ -47,4 +59,4 @@ done
 # cdo reducegrid,$lat_mask $IN_PATH/$MODEL_PATH/DW-ATM/atmos/15min/clivi/r1i1p1f1/2d/gn/clivi_15min_SCREAM-3km_DW-ATM_r1i1p1f1_2d_gn_20200223000000-20200223234500.nc $out
 
 ### solar insolation from Ben Hillman ###
-# cdo -f nc -sellonlatbox,$LON0,$LON1,$LAT0,$LAT1 -setgrid,$GRID_FILE -seldate,2020-01-30T00:00:00,2020-03-01T23:59:00 /work/bb1153/b380883/SCREAM_global_SOLIN_20200120-20200301.nc /work/bb1153/b380883/TWP/TWP_SCREAM_rsdt_20200130-20200301.nc
+# cdo -f nc -sellonlatbox,$LON0,$LON1,$LAT0,$LAT1 -setgrid,$GRID_FILE -seldate,2020-01-30T00:00:00,2020-03-01T23:59:00 /work/bb1153/b380883/gn_SCREAM_20200120-20200301.nc /work/bb1153/b380883/TWP/TWP_SCREAMnative_rsdt_20200130-20200301.nc
