9339ecb79d28ca11e87566c3a411428213233508
Samantha <smturbev@uw.edu>
2023-03-08 18:25:42 +0100

    meridional mean for gt olr

--- sh_scripts/fldmean.sh
+++ sh_scripts/fldmean.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 #SBATCH --job-name=fldmean
 #SBATCH --partition=compute
-#SBATCH --mem=20GB
+#SBATCH --mem=100GB
 #SBATCH --time=04:00:00
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=smturbev@uw.edu
 #SBATCH --account=bb1153
-#SBATCH --output=out_fldmean.eo
-#SBATCH --error=err_fldmean.eo
+#SBATCH --output=out_fldmean%j.eo
+#SBATCH --error=err_fldmean%j.eo
 
 set -evx # verbose messages and crash message
 twp=/work/bb1153/b380883/TWP
+gt=/work/bb1153/b380883/GT
 
-cdo -fldmean $twp/TWP_3D_UM_totalwater_20200130-20200228.nc $twp/mean/fldmean_TWP_3D_UM_totalwater_20200130-20200228.nc
-cdo -fldmean $twp/TWP_3D_SCREAM_totalwater_20200130-20200228.nc $twp/mean/fldmean_TWP_3D_SCREAM_totalwater_20200130-20200228.nc
-cdo -fldmean $twp/TWP_3D_ARP_totalwater_20200130-20200228.nc $twp/mean/fldmean_TWP_3D_ARP_totalwater_20200130-20200228.nc
-cdo -fldmean $twp/TWP_3D_SHiELD_totalwater_20200130-20200228.nc $twp/mean/fldmean_TWP_3D_SHiELD_totalwater_20200130-20200228.nc
-cdo -fldmean $twp/TWP_3D_ICON_totalwater_20200130-20200228.nc $twp/mean/fldmean_TWP_3D_ICON_totalwater_20200130-20200228.nc
-
-cdo -fldmean $twp/TWP_3D_UM_cl_5e-7kgm-3_20200130-20200228.nc $twp/mean/fldmean_TWP_3D_UM_cl_5e-7kgm-3_20200130-20200228.nc
-cdo -fldmean $twp/TWP_3D_SCREAM_cl_5e-7kgm-3_20200130-20200228.nc $twp/mean/fldmean_TWP_3D_SCREAM_cl_5e-7kgm-3_20200130-20200228.nc
-cdo -fldmean $twp/TWP_3D_ARP_cl_5e-7kgm-3_20200130-20200228.nc $twp/mean/fldmean_TWP_3D_ARP_cl_5e-7kgm-3_20200130-20200228.nc
-cdo -fldmean $twp/TWP_3D_SHiELD_cl_5e-7kgm-3_20200130-20200228.nc $twp/mean/fldmean_TWP_3D_SHiELD_cl_5e-7kgm-3_20200130-20200228.nc
-cdo -fldmean $twp/TWP_3D_ICON_cl_5e-7kgm-3_20200130-20200228.nc $twp/mean/fldmean_TWP_3D_ICON_cl_5e-7kgm-3_20200130-20200228.nc
+cdo -mermean $gt/GT_ARPr1deg_rlt_20200130-20200228.nc $gt/fldmean/fldmean_GT_ARPr1deg_rlt_20200130-20200228.nc
+cdo -mermean $gt/GT_GEOSr1deg_rlut_20200130-20200228.nc $gt/fldmean/fldmean_GT_GEOSr1deg_rlut_20200130-20200228.nc
+cdo -mermean $gt/GT_ICONr1deg_rlt_20200130-20200228.nc $gt/fldmean/fldmean_GT_ICONr1deg_rlt_20200130-20200228.nc
+cdo -mermean $gt/GT_SAMr1deg_rlt_20200130-20200228.nc $gt/fldmean/fldmean_GT_SAMr1deg_rlt_20200130-20200228.nc
+cdo -mermean $gt/GT_SCREAMr1deg_rlt_20200130-20200228.nc $gt/fldmean/fldmean_GT_SCREAMr1deg_rlt_20200130-20200228.nc
+cdo -mermean $gt/GT_SHiELDr1deg_rlut_20200130-20200228.nc $gt/fldmean/fldmean_GT_SHiELDr1deg_rlut_20200130-20200228.nc
+cdo -mermean $gt/GT_UM_rlut_20200130-20200228.nc $gt/fldmean/fldmean_GT_UM_rlut_20200130-20200228.nc
+cdo -mermean $gt/GT_CERES_rad_toa_1hm_JFM.nc $gt/fldmean/fldmean_GT_CERES_rad_toa_1hm_JFM.nc
 
 echo "done"
