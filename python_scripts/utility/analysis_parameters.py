d42d615fa1d0947890ff0e752565e580df528d3c
Samantha <smturbev@uw.edu>
2023-03-29 20:06:44 +0200

    adding load functions for olr, swu, swd, alb

--- python_scripts/utility/analysis_parameters.py
+++ python_scripts/utility/analysis_parameters.py
@@ -122,11 +122,12 @@ def get_fldmean_file(model, region="twp", var="pr"):
     elif region.lower()=="twp":
         return TWP+"mean/fldmean_TWP_3D_"+model+"_"+var.lower()+"_20200130-20200228.nc"
     return
-def get_mermean_file(model, region,="gt", var="rlt"):
+
+def get_mermean_file(model, region="gt", var="rlt"):
     if model=="CERES":
-            return GT+"fldmean/mermean_GT_CERES_rad_toa_1hm_JFM.nc"
-        else:
-            return GT+"fldmean/mermean_GT_{m}_{v}_20200130-20200228.nc".format(m=model, v=var)
+        return GT+"fldmean/mermean_GT_CERES_rad_toa_1hm_JFM.nc"
+    else:
+        return GT+"fldmean/mermean_GT_{m}_{v}_20200130-20200228.nc".format(m=model, v=var)
 
 def get_xytmean_file(model, region="TWP", var="zg"):
     return TWP+"mean/xytmean_"+region+"_3D_"+model+"_zg_20200130-20200228.nc"
@@ -146,5 +147,134 @@ def open_file(model, region="twp", var="rlut", timmean=False, fldmean=False, fld
 
 def open_dyamond1(model, region="TWP", var="rlut"):
     return xr.open_dataset(TWP+"dyamond1/{}_{}_{}_20160810-20160910.nc".format(region, model, var))
-    
 
+def load_olr(model, region="TWP",r=0):
+    """ get the OLR radiation variable in W/m2 for given model and region for specified regridded resolution
+        r=0 for native grid, r=0.1 for 0.1 deg remapcon, r=1 for 1 deg remapcon
+    """
+    chunk_dict = {"time":500, "lat":400, "lon":1440} # "grid_size":500000}
+    if r>0:
+        # housekeeping - make sure r=0.1 for TWP region, and set up chunk dictionary for dask
+        if (region=="TWP") & (r!=0.1):
+            raise Exception("r={} not defined for {} region.".format(r,region))
+        # models
+        if (model=="SCREAM") or (model=="ARP") or (model=="SAM"):
+            # rlt r0.1 deg
+            olr = xr.open_dataset(ap.get_file("{}r{}deg".format(model, r), region, "rlt"), chunks=chunk_dict).rlt
+        elif (model=="GEOS") or (model=="SHiELD"):
+            # rlut r0.1 deg
+            olr = xr.open_dataset(ap.get_file("{}r{}deg".format(model, r), region, "rlut"), chunks=chunk_dict).rlut
+        elif (model=="ICON"):
+            # ICON needs extra care due to first hour messed up from accumulation
+            olr = xr.open_dataset(ap.get_file("ICONr{}deg".format(r), region, "rlt"), chunks=chunk_dict).rlt
+            olr = olr.where(olr.time.dt.hour!=0) # first hour is messed up b/c of accumulation
+        elif (model=="UM"):
+            # native grid coarsened to 0.1 deg to match other models
+            olr = xr.open_dataset(ap.get_file("UM",region,"rlut"), chunks=chunk_dict).rlut.coarsen(latitude=int(214/100), longitude=int(142/100), boundary='trim').mean()
+        else:
+            raise Exception("model or region not defined properly. Try 'TWP' or 'SCREAM', 'UM', 'GEOS', etc.")
+    else: #native grid
+        if (model=="SCREAM"):
+            olr = xr.open_dataset(ap.get_file(model, region, "rlt"), chunks={"time":500, "ncol":1000, "grid_size":1000}).rlt
+        elif (model=="ARP") or (model=="IFS"): #rlt native grid
+            olr = xr.open_dataset(ap.get_file(model, region, "rlt"), chunks={"time":500,"rgrid":1000}).rlt
+        elif (model=="SAM"):
+            olr = xr.open_dataset(ap.get_file(model, region, "rlt"), chunks={"time":500,"latlon":1000}).rlt
+        elif (model=="GEOS") or (model=="SHiELD"): #rlut native grid
+            olr = xr.open_dataset(ap.get_file(model, region, "rlut"), chunks={"time":500,"Xdim":1000}).rlut
+        elif (model=="UM"):
+            olr = xr.open_dataset(ap.get_file(model,region,"rlut"), chunks=chunk_dict).rlut
+        elif (model=="ICON"):
+            # ICON needs extra care due to first hour messed up from accumulation
+            olr = xr.open_dataset(ap.get_file("ICON", region, "rlt"), chunks={"time":500, "ncell":1000}).rlt
+            olr = olr.where(olr.time.dt.hour!=0) # first hour is messed up b/c of accumulation
+        elif (model=="MPAS"):
+            olr = xr.open_dataset(ap.get_file("MPAS", region, "rlt"), chunks={"xtime":500, "nCells":1000}).rlt
+        else:
+            raise Exception("model or region not defined properly. Try 'TWP' or 'SCREAM', 'UM', 'GEOS', etc.")
+    return olr
+
+def load_swu_swd(model, region="TWP",r=0):
+    """ returns a tuple of xarrays of (SW upward, SW downward) radiation variable in W/m2 for given model and region for specified regridded resolution
+        r=0 for native grid, r=0.1 for 0.1 deg remapcon, r=1 for 1 deg remapcon
+    """
+    
+    chunk_dict = {"time":500, "lat":400, "lon":1440} # "grid_size":500000}
+    if r>0:
+        # housekeeping - make sure r=0.1 for TWP region, and set up chunk dictionary for dask
+        if (region=="TWP") & (r!=0.1):
+            raise Exception("r={} not defined for {} region.".format(r,region))
+        # models
+        if (model=="SCREAM") or (model=="ARP") or (model=="SAM"):
+            # rst r0.1 deg
+            swn = xr.open_dataset(ap.get_file("{}r{}deg".format(model, r), region, "rst"), chunks=chunk_dict).rst
+        elif (model=="GEOS") or (model=="SHiELD"):
+            # rlst r0.1 deg
+            swu = xr.open_dataset(ap.get_file("{}r{}deg".format(model, r), region, "rsut"), chunks=chunk_dict).rsut
+            swd = xr.open_dataset(ap.get_file("{}r{}deg".format(model, r), region, "rsdt"), chunks=chunk_dict).rsdt
+        elif (model=="ICON"):
+            # ICON needs extra care due to first hour messed up from accumulation
+            swu = xr.open_dataset(ap.get_file("ICONr{}deg".format(r), region, "rsut"), chunks=chunk_dict).rsut
+            swn = xr.open_dataset(ap.get_file("ICONr{}deg".format(r), region, "rst"), chunks=chunk_dict).rst
+            swu = swu.where(swu.time.dt.hour!=0) # first hour is messed up b/c of accumulation
+            swn = swn.where(swn.time.dt.hour!=0)
+            swd = swn + swu.values
+        elif (model=="UM"):
+            # native grid coarsened to 0.1 deg to match other models
+            swu = xr.open_dataset(ap.get_file(model, region, "rsut"), chunks=chunk_dict).rsut.coarsen(latitude=int(214/100), longitude=int(142/100), boundary='trim').mean()
+            swd = xr.open_dataset(ap.get_file(model, region, "rsdt"), chunks=chunk_dict).rsdt.coarsen(latitude=int(214/100), longitude=int(142/100), boundary='trim').mean()
+        else:
+            raise Exception("model or region not defined properly. Try 'TWP' or 'SCREAM', 'UM', 'GEOS', etc.")
+    else: #native grid
+        if (model=="SCREAM"):
+            swn = xr.open_dataset(ap.get_file(model, region, "rst"), chunks={"time":500, "ncol":1000, "grid_size":1000}).rst.rename({"grid_size":"ncol"})
+            swd = xr.open_dataset(ap.get_file("SCREAM", "TWP", "rsdt"), chunks=chunk_dicts).SOLIN
+            swu = swd-swn
+        elif (model=="ARP") or (model=="IFS"): # 
+            swn = xr.open_dataset(ap.get_file(model, region, "rst"), 
+                                  chunks={"time":500,"rgrid":1000}).rst
+            swd_ex = xr.open_dataset(ap.get_file("GEOS", region, "rsdt"), 
+                                     chunks={"time":500,"Xdim":1000}).rsdt.rename({"Xdim":"rgrid"})
+            swd = swd_ex.interp(time=swn.time,
+                       rgrid=swn.rgrid, 
+                       method="nearest",
+                       kwargs={"fill_value": np.nan})
+            swu = (swd - swn)
+        elif (model=="SAM"):
+            swn = xr.open_dataset(ap.get_file(model, region, "rst"), 
+                                  chunks={"time":500,"latlon":1000}).rst
+            swd_ex = xr.open_dataset(ap.get_file("GEOS", region, "rsdt"), 
+                                     chunks={"time":500,"Xdim":1000}).rsdt.rename({"Xdim":"rgrid"})
+            swd = swd_ex.interp(time=swn.time,
+                       rgrid=swn.rgrid, 
+                       method="nearest",
+                       kwargs={"fill_value": np.nan})
+            swu = (swd - swn)
+        elif (model=="GEOS") or (model=="SHiELD"): #rlut native grid
+            swu = xr.open_dataset(ap.get_file(model, region, "rsut"), chunks={"time":500,"Xdim":1000}).rsut
+            swd = xr.open_dataset(ap.get_file(model, region, "rsdt"), chunks={"time":500,"Xdim":1000}).rsdt
+        elif (model=="UM"):
+            swu = xr.open_dataset(ap.get_file(model,region,"rsut"), chunks=chunk_dict).rsut
+            swd = xr.open_dataset(ap.get_file(model,region,"rsdt"), chunks=chunk_dict).rsdt
+        elif (model=="ICON"):
+            # ICON needs extra care due to first hour messed up from accumulation
+            swu = xr.open_dataset(ap.get_file("ICON", region, "rsut"), chunks=chunk_dict).rsut
+            swn = xr.open_dataset(ap.get_file("ICON", region, "rst"), chunks=chunk_dict).rst
+            swu = swu.where(swu.time.dt.hour!=0) # first hour is messed up b/c of accumulation
+            swn = swn.where(swn.time.dt.hour!=0)
+            swd = swn + swu.values
+        elif (model=="MPAS"):
+            raise Exception("MPAS model not yet ready to use...")
+            # swn = xr.open_dataset(ap.get_file("MPAS", region, "rst"), chunks={"xtime":500, "nCells":1000}).rst
+            # to do
+            # calculate swd interpolated to xtime and nCells (maybe convert first to datetime)
+        else:
+            raise Exception("model or region not defined properly. Try 'TWP' or 'SCREAM', 'UM', 'GEOS', etc.")
+    print("returning (SWU,SWD)")
+    return (swu, swd)
+
+def load_alb(model, region="TWP", r=0):
+    """ returns albedo of a given model and region for specified resolution (r=0 is native grid) """
+    swu, swd = get_swu_swd(model, region, r)
+    alb = swu/swd.where(swd>100).values
+    return alb
